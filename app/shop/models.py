d7f3f096685536c76f18a3c9e6b9cbba6e42162f
vladbevziuk <vladbevziuk71@gmail.com>
2024-04-10 17:32:03 +0200

    create models: category and course for saving courses in category

--- app/shop/models.py
+++ app/shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    students_amount = models.IntegerField()
+    reviews_amount = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
