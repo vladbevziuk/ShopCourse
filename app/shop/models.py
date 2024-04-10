26efd893563b32bbbd6b37a431beacc4dcb8e955
vladbevziuk <vladbevziuk71@gmail.com>
2024-04-10 23:03:27 +0200

    adding models function __str__ for showing title

--- app/shop/models.py
+++ app/shop/models.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 class Category(models.Model):
     title = models.CharField(max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
+    def __str__(self):        # it's for vievs not objects only name this title 
+        return self.title
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
@@ -12,4 +14,6 @@ class Course(models.Model):
     reviews_amount = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
+    def __str__(self):  # its same like function in category
+        return self.title 
 
--- app/shop/views.py
+++ app/shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Course
 
-
+def index(requet):
+    courses = Course.objects.all()
+    return HttpResponse(''.join([str(course) + '<br>' for course in courses])) 
