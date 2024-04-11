a6fa905dcc957c544aa4be098a598a4362fc5fee
vladbevziuk <vladbevziuk71@gmail.com>
2024-04-11 16:45:04 +0200

    create templates

--- app/shop/views.py
+++ app/shop/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Course
 
-def index(requet):
+def index(request):
     courses = Course.objects.all()
-    return HttpResponse(''.join([str(course) + '<br>' for course in courses])) 
+    return render(request, "index.html", {'courses': courses})
