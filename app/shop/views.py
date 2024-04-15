d05009c8165de72e1c52b0670540b52aeb773640
vladbevziuk <vladbevziuk71@gmail.com>
2024-04-15 23:37:41 +0200

    added new html file for show course, and to fix problems

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
 from .models import Course
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, "index.html", {'courses': courses})
+    return render(request, "shop/index.html", {'courses': courses})
+
+def single_course(request, course_id):
+    # try:
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, 'course.html', {'course': course})
+    # except Course.DoesNotExist:
+    #     raise Http404()
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'shop/course.html', {'course': course})
+
